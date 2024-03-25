5fa85844d08c2c705418c90865f6b4f253f03adc
Lauren Yeung <laurenyeung217@gmail.com>
2024-03-25 18:35:26 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1, num2);
-    } else {
+    } else if (gameType === "subtract"){
+        displaySubtractQuestion(num1, num2);
+    }else {
     alert(`Unknown game type: ${gameType}`);
     //throw statement will stop the game form running
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -77,6 +79,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x"){
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-"){
+        return [operand1 - operand2, "subtract"]
     }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -113,8 +117,11 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    //Which is bigger: operand1 or operand2?
+    //If operand1 is bigger, return that.
+    //If operand2 is bigger, return that instead.
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
